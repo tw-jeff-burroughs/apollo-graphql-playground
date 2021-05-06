2ce6ff7c736c008942cc51c1a85618d79cd825b7
Jeff Burroughs <jburroughs@sparkimaginations.com>
2021-05-06 18:08:05 -0500

    extend endpoint

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ const { RESTDataSource } = require('apollo-datasource-rest');
 class NASAAPI extends RESTDataSource {
   constructor() {
     super();
-    this.baseURL = 'https://api.nasa.gov/planetary'
+    this.baseURL = 'https://api.nasa.gov'
   }
 
   async getApod() {
     return this.get(
-        `apod?api_key=${process.env.NASA}`);
+        `planetary/apod?api_key=${process.env.NASA}`);
+  }
+
+  async getNEOs() {
+    return this.get(
+        `neo/rest/v1/neo/browse?page=0&size=20&api_key=${process.env.NASA}`
+    )
   }
 }
 
@@ -31,80 +37,8 @@ class ISSAPI extends RESTDataSource {
   }
 }
 
-const libraries = [
-  {
-    branch: 'downtown'
-  },
-  {
-    branch: 'riverside'
-  },
-];
-
-// The branch field of a book indicates which library has it in stock
-const books = [
-  {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
-    branch: 'riverside'
-  },
-  {
-    title: 'City of Glass',
-    author: 'Paul Auster',
-    branch: 'downtown'
-  },
-];
-
-const authors = [
-  {
-    name: 'Kate Chopin',
-    favorite_pie: 'lemon',
-  },
-  {
-    name: 'Paul Auster',
-    favorite_pie: 'cherry',
-  }
-]
-
-const titles = [
-  {
-    name: 'City of Glass',
-    pages: 452,
-    ebook: false
-  },
-  {
-    name: 'The Awakening',
-    pages: 451,
-    ebook: true
-  }
-]
-
 // Schema definition
 const typeDefs = gql`
-
-# A library has a branch and books
-  type Library {
-    branch: String!
-    books: [Book!]
-  }
-
-  # A book has a title and author
-  type Book {
-    title: Title!
-    author: Author!
-  }
-
-  # An author has a name
-  type Author {
-    name: String!
-    favorite_pie: String!
-  }
-  
-  type Title {
-    name: String!
-    pages: Int!
-    ebook: Boolean!
-  }
-  
   type Satellite {
     name: String!
     id: Int!
@@ -131,12 +65,26 @@ const typeDefs = gql`
     title: String!
     url: String!
   }
+  
+  type Link {
+    self: String!
+  }
+  
+  type NEO {
+    near_earth_objects: [NEO_BODY]
+  }
+  
+  type NEO_BODY {
+    links: Link
+    id: String!
+    name: String!
+  }
 
   # Queries can fetch a list of libraries
   type Query {
-    libraries: [Library]
     satellites: [Satellite]
     locations: [Location]
+    neos: NEO
     apod: APOD
   }
 `;
@@ -144,10 +92,6 @@ const typeDefs = gql`
 // Resolver map
 const resolvers = {
   Query: {
-    libraries() {
-      // Return our hardcoded array of libraries
-      return libraries;
-    },
     async satellites(_, __, { dataSources }) {
       return dataSources.issAPI.getSatellites();
     },
@@ -156,29 +100,11 @@ const resolvers = {
     },
     async apod(_, __, { dataSources }) {
       return dataSources.nasaAPI.getApod();
-    }
-  },
-  Library: {
-    books(parent) {
-
-      // Filter the hardcoded array of books to only include
-      // books that are located at the correct branch
-      return books.filter(book => book.branch === parent.branch);
-    }
-  },
-  Book: {
-    author(parent) {
-      return authors.filter(author => author.name === parent.author)[0];
     },
-    title(parent) {
-      return titles.filter(title => title.name === parent.title)[0];
+    async neos(_, __, { dataSources }) {
+      return dataSources.nasaAPI.getNEOs();
     }
-  }
-
-
-  // Because Book.author returns an object with a "name" field,
-  // Apollo Server's default resolver for Author.name will work.
-  // We don't need to define one.
+  },
 };
 
 // Pass schema definition and resolvers to the
